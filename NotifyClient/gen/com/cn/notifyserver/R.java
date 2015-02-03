2a1941a5e66e02335aae3e970811cc93063a5068
cnvill <cn_vill@hotmail.com>
2015-02-03 00:30:47 +0000

    Add Menu Setting

--- NotifyClient/gen/com/cn/notifyserver/R.java
+++ NotifyClient/gen/com/cn/notifyserver/R.java
@@ -13,8 +13,14 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int item=0x7f070000;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
+    }
+    public static final class menu {
+        public static final int menu_client=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
@@ -22,8 +28,5 @@ public final class R {
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
-    }
-	public static final class menu {
-        public static final int main=0x7f0c0000;
     }
 }
